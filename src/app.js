e08f0b4b7446edb568850498cdbb5d69457895fc
lanterva <lanterva@gmail.com>
2026-09-01 19:41:24 +0900

    feat: 쿠키 클래스
    쿠키 추가 & 받기 기능 작성

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import { Cookie } from './server/cookie.js'
 import { Router } from './server/router.js'
 import { Server } from './server/server.js'
 
@@ -5,7 +6,18 @@ const server = new Server()
 const router1 = new Router()
 
 router1.get('/', (req, res) => {
+  Cookie.setCookie(res, 'name', 'd', {
+    MaxAge: 1200,
+    HttpOnly: true,
+  })
   res.json({ msg: '반갑습니다.' })
+})
+
+router1.get('/c', (req, res) => {
+  const cookie = Cookie.getCookie(req, 'name')
+  console.log(cookie.name)
+  console.log(cookie.value)
+  res.json(Cookie.getCookie(req, 'name'))
 })
 
 server.router('/ra', router1)
